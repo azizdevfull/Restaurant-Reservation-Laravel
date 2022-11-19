692a66618bf0e5a75594460ae5d6d82ffe04de74
azizdevfull <aziz16110904@gmail.com>
2022-11-19 14:10:12 +0500

    completed menu and categorys' belongstomany and create menu's view page

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Menu;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'image'];
+
+    public function menus()
+    {
+        return $this->belongsToMany(Menu::class, 'category_menu');
+    }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Menu extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_menu');
+    }
 }
