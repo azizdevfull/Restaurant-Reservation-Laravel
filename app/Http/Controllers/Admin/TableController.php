0322f0ca01d0d4c2a1cf15af7ae51d41d4874c12
azizdevfull <aziz16110904@gmail.com>
2022-11-19 14:58:38 +0500

    completed table update and destroy

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -64,9 +64,9 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.tables.edit',compact('table'));
     }
 
     /**
@@ -76,9 +76,11 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TableStoreRequest $request, Table $table)
     {
-        //
+        $table->update($request->validated());
+
+        return to_route('admin.tables.index');
     }
 
     /**
@@ -87,8 +89,10 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Table $table)
     {
-        //
+        $table->delete();
+
+        return to_route('admin.tables.index');
     }
 }
